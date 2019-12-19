0c1c942e6a21a841424b6b105611361f39264c34
LAPTOP-FKOQDFD9\李荣亮 <1843306093@.qq.com>
2019-12-19 09:29:31 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -2,7 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import service from "./http"
+import dayjs from 'dayjs'
 
+
+Vue.use(ElementUI)
+
+// 把service对象挂载在Vue的原型对象上
+// 每一个组件都可以使用
+Vue.prototype.$axios = service
+Vue.prototype.$dayjs = dayjs
 Vue.config.productionTip = false
 
 new Vue({
